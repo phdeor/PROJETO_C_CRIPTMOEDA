3e002cef884d5a9dbeb60172a733e907101f8ed9
phdeor <163015480+phdeor@users.noreply.github.com>
2024-09-26 23:39:42 -0300

    Update login.c

--- login.c
+++ login.c
@@ -34,50 +34,6 @@ void login() {
     }
 }
 
-int main() {
-   
-    login();
-    main_menu();
-    consultar_saldo();
-    depositar();
-    return 0;
-}
-
-#include <stdio.h>
-#include <string.h>
-#include "funcao.h"
-
-#define CPF "123456789"
-#define SENHA "12345"
-#define USUARIO "Souza_henrique_delfino"
-
-void login() {
-    char user[20];
-    char password[20];
-
-    while (1) {
-        printf("\nLOGIN\n");
-
-        printf("Digite o seu CPF: ");
-        fgets(user, sizeof(user), stdin);
-        user[strcspn(user, "\n")] = '\0';
-
-        printf("Digite sua senha: ");
-        fgets(password, sizeof(password), stdin);
-        password[strcspn(password, "\n")] = '\0';
-
-        printf("\n");
-
-        if (strcmp(CPF, user) == 0 && strcmp(SENHA, password) == 0) {
-            printf("\n");
-            printf("Login efetuado com sucesso\n");
-            break;
-        } else {
-            printf("\n");
-            printf("Senha ou usuario incorreto\n");
-        }
-    }
-}
 
 int main() {
    
@@ -85,51 +41,7 @@ int main() {
     main_menu();
     consultar_saldo();
     depositar();
-    return 0;
-}
-
-#include <stdio.h>
-#include <string.h>
-#include "funcao.h"
-
-#define CPF "123456789"
-#define SENHA "12345"
-#define USUARIO "Souza_henrique_delfino"
-
-void login() {
-    char user[20];
-    char password[20];
-
-    while (1) {
-        printf("\nLOGIN\n");
-
-        printf("Digite o seu CPF: ");
-        fgets(user, sizeof(user), stdin);
-        user[strcspn(user, "\n")] = '\0';
-
-        printf("Digite sua senha: ");
-        fgets(password, sizeof(password), stdin);
-        password[strcspn(password, "\n")] = '\0';
-
-        printf("\n");
-
-        if (strcmp(CPF, user) == 0 && strcmp(SENHA, password) == 0) {
-            printf("\n");
-            printf("Login efetuado com sucesso\n");
-            break;
-        } else {
-            printf("\n");
-            printf("Senha ou usuario incorreto\n");
-        }
-    }
-}
-
-int main() {
-   
-    login();
-    main_menu();
-    consultar_saldo();
-    depositar();
+    sacar();
     return 0;
 }
 

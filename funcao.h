6c95189bb88d2beb3d9ca562463957b40184e689
Gustavo Souza Alvarenga <167848709+Souz1n@users.noreply.github.com>
2024-10-01 23:35:22 -0300

    Update funcao.h _FINAL

--- funcao.h
+++ funcao.h
@@ -1,10 +1,55 @@
-#ifndef funcao_h
-#define funcao_h
+#ifndef FUNCAO_H
+#define FUNCAO_H
 
-void main_menu();
-void consultar_saldo();
-void depositar();
+#define MAX_TRANSACOES 100
+#define NUM_USUARIOS 10
 
+typedef struct {
+    char data_hora[20];
+    char tipo[20];
+    float valor;
+    char cripto[20];
+    float cotacao;
+    float tx;
+    float reais;
+    float bitcoin;
+    float ethereum;
+    float ripple;
+} Transacao;
 
+typedef struct {
+    char nome[50];
+    char cpf[13];
+    char senha[20];
+    float reais;
+    float bitcoin;
+    float ethereum;
+    float ripple;
+    Transacao transacoes[MAX_TRANSACOES];
+    int num_transacoes;
+} USUARIO;
+
+extern USUARIO usuarios_fixos[NUM_USUARIOS];
+
+typedef struct {
+    char nome[20];
+    float cotacao;
+} Criptomoeda;
+
+extern Criptomoeda CT[3];
+
+// Protótipos das funções
+void inicializaUsuario(USUARIO usuario, const charnome, const char cpf, float reais, float bitcoin, float ethereum, float ripple);
+void menu(USUARIOusuario);
+void consultar_saldo(USUARIO usuario);
+void consultar_extrato(USUARIOusuario);
+void depositar(USUARIO usuario);
+void sacar(USUARIOusuario);
+void comprar_criptomoedas(USUARIO usuario);
+void vender_criptomoedas(USUARIOusuario);
+void atualizar_cotacao();
+void gerar_extrato(USUARIO usuario); 
+void salvar_dados(USUARIO usuarios[], int num_usuarios);
+void carregar_dados(USUARIO usuarios[], intnum_usuarios);
 
 #endif
